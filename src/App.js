c7443d69cb2d5dac23d197b8008e203093fdd989
rodolphe37 <rodolphe.a@gmail.com>
2019-11-23 15:02:24 +0100

    base saine react and p5js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
 import sketch2 from './sketches/sketch2';
 import './App.css';
+import SketchPageDemo from './SketchPageDemo';
 
 function App(){
     return (
       <div>
+          <SketchPageDemo />
         <P5Wrapper sketch={sketch2} ></P5Wrapper>
       </div>
     );
--- src/sketches/sketch2.js
+++ src/sketches/sketch2.js
@@ -1,16 +1,12 @@
 export default function sketch(p){
-    let canvas;
+
 
     p.setup = () => {
-      canvas = p.createCanvas(400, 400);
+      p.createCanvas(400, 400);
     }
 
     p.draw = () => {
       p.background(220,0,200);
     };
 
-    p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
-        if(canvas) //Make sure the canvas has been created
-          p.fill(newProps.color);
-      }
 };
