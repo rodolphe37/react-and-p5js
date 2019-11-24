ad2d8e28161337738d5b1b44d6682cac70b79c68
rodolphe37 <rodolphe.a@gmail.com>
2019-11-24 16:39:38 +0100

    base saine react + p5js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
-import sketch2 from './sketches/sketch2';
+import sketch from './sketches/sketch';
 import './App.css';
-import SketchPageDemo from './SketchPageDemo';
 
 function App(){
     return (
       <div>
-          <SketchPageDemo />
-        <P5Wrapper sketch={sketch2} ></P5Wrapper>
+        <P5Wrapper sketch={sketch} ></P5Wrapper>
       </div>
     );
 }
