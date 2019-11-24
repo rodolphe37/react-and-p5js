d135f26f6c13f7b4fad10dde040da2a15c91bf9c
rodolphe37 <rodolphe.a@gmail.com>
2019-11-24 10:00:38 +0100

    first step

--- src/sketches/sketch2.js
+++ src/sketches/sketch2.js
@@ -6,7 +6,33 @@ export default function sketch(p){
     }
 
     p.draw = () => {
-      p.background(220,0,200);
+      p.background(0);
+
+      //p.rect(175, 0, 75, 150);
+
+      let hr = p.hour();
+      let mn = p.minute();
+      let sc = p.second();
+
+      p.strokeWeight(8);
+      p.noFill();
+      p.stroke(255, 100, 150);
+      let end1 = p.map(sc, 0, 60, 0, 360);
+      p.arc(200, 200, 300, 300, 0, end1);
+
+      p.stroke(150, 100, 255);
+      let end2 = p.map(mn, 0, 60, 0, 360);
+      p.arc(200, 200, 280, 280, 0, end2);
+
+      p.stroke(150, 255, 100);
+      let end3 = p.map(hr % 12, 0, 12, 0, 360);
+      p.arc(200, 200, 260, 260, 0, end3);
+
+      
+
+      //p.fill(255);
+      //p.noStroke();
+      //p.text(hr + ':' + mn + ':' + sc, 10, 200);
     };
 
 };
