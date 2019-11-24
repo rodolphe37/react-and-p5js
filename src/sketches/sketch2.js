25fabc95cdd87db90724c8eb3dd8379e6ccb2cdb
rodolphe37 <rodolphe.a@gmail.com>
2019-11-24 10:26:43 +0100

    second step

--- src/sketches/sketch2.js
+++ src/sketches/sketch2.js
@@ -7,6 +7,8 @@ export default function sketch(p){
 
     p.draw = () => {
       p.background(0);
+      p.translate(200, 200);
+      p.rotate(-90);
 
       //p.rect(175, 0, 75, 150);
 
@@ -17,18 +19,37 @@ export default function sketch(p){
       p.strokeWeight(8);
       p.noFill();
       p.stroke(255, 100, 150);
-      let end1 = p.map(sc, 0, 60, 0, 360);
-      p.arc(200, 200, 300, 300, 0, end1);
+      let secondAngle = p.map(sc, 0, 60, 0, 360);
+      p.arc(0, 0, 300, 300, 0, secondAngle);
+
+      p.push();
+      p.rotate(secondAngle);
+      p.stroke(255, 100, 150);
+      p.line(0, 0, 100, 0);
+      p.pop();
 
       p.stroke(150, 100, 255);
-      let end2 = p.map(mn, 0, 60, 0, 360);
-      p.arc(200, 200, 280, 280, 0, end2);
+      let minuteAngle = p.map(mn, 0, 60, 0, 360);
+      p.arc(0, 0, 280, 280, 0, minuteAngle);
+
+      p.push();
+      p.rotate(minuteAngle);
+      p.stroke(150, 100, 255);
+      p.line(0, 0, 75, 0);
+      p.pop();
 
       p.stroke(150, 255, 100);
-      let end3 = p.map(hr % 12, 0, 12, 0, 360);
-      p.arc(200, 200, 260, 260, 0, end3);
+      let hourAngle = p.map(hr % 12, 0, 12, 0, 360);
+      p.arc(0, 0, 260, 260, 0, hourAngle);
 
-      
+      p.push();
+      p.rotate(hourAngle);
+      p.stroke(150, 255, 100);
+      p.line(0, 0, 50, 0);
+      p.pop();
+
+      p.stroke(255);
+      p.point(0,0)
 
       //p.fill(255);
       //p.noStroke();
