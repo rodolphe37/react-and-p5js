4b64ee52cf4c68ad2850f49d15b5c273c271183c
rodolphe37 <rodolphe.a@gmail.com>
2019-11-24 16:17:57 +0100

    fibonacci to flower

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import P5Wrapper from 'react-p5-wrapper';
-import sketch2 from './sketches/sketch2';
-import sketch3 from './sketches/sketch3';
+//import sketch2 from './sketches/sketch2';
+//import sketch3 from './sketches/sketch3';
 import sketch4 from './sketches/sketch4';
 import './App.css';
-import SketchPageDemo from './SketchPageDemo';
+//import SketchPageDemo from './SketchPageDemo';
 
 function App(){
     return (
       <div>
         <P5Wrapper sketch={sketch4}></P5Wrapper>
-        <SketchPageDemo />
-        <div className="rotate-canvas">
+        {/*<SketchPageDemo />*/}
+        {/*<div className="rotate-canvas">
           <P5Wrapper sketch={sketch2} ></P5Wrapper>
         </div>
-          <P5Wrapper sketch={sketch3} ></P5Wrapper>
+    <P5Wrapper sketch={sketch3} ></P5Wrapper>*/}
       </div>
     );
 }
--- src/sketches/sketch4.js
+++ src/sketches/sketch4.js
@@ -1,4 +1,3 @@
-import p5 from 'p5';
 
 function sketch4(p5){
     let n = 0;
